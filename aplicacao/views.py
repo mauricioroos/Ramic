06f8063777d7814872d8293af63352913c871ad4
unknown <fruhaufkauan@gmail.com>
2025-08-19 15:16:27 -0300

    adicionado a tabela monitoramento do banco de dados

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Produto
+from .models import Monitoriamento
 
 # Create your views here.
 def produtos(request):
     produtos = Produto.objects.all()
     context = {
         'produtos' : produtos,
+    }
+
+def monitoramento(request):
+    monitoramento = Monitoriamento.objects.all()
+    context = {
+        'monitoramento' : monitoramento,
     }
 
 def index(request):
